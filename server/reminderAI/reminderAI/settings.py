6424cbb4df7ce3593827575627688d0e245aca11
SpaceKwazzart <vladimirbeliaev3@gmail.com>
2024-03-16 09:18:36 +0300

    Connect postgres and mongo, add to docker-ompose

--- server/reminderAI/reminderAI/settings.py
+++ server/reminderAI/reminderAI/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR.parent, ".env"))
+READ_ENVS_FROM_FILE = load_dotenv(os.path.join(BASE_DIR.parent, ".env"))
 
 ADMIN_NAME = os.getenv("ADMIN_NAME")
 ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
@@ -86,10 +87,22 @@ ASGI_APPLICATION = "reminderAI.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+mongo_client = MongoClient(
+    host=os.getenv("MONGO_HOST"),
+    port=int(os.getenv("MONGO_PORT")),
+    username=os.getenv("MONGO_USER"),
+    password=os.getenv("MONGO_PASSWORD"),
+)
+mongo_db_handle = mongo_client["db_name"]
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST") if not READ_ENVS_FROM_FILE else "localhost",
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
